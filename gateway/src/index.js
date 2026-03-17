b0e62f30e57a9d36b88fcef04bc76e248cd69a27
LeonXu <desempty@gmail.com>
2026-03-17 21:07:33 +0800

    chore: 发布 v1.5.11

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -22,8 +22,8 @@ app.options('*', (c) => c.body(null, 204));
  */
 app.get('/version', (c) => {
     return c.json({
-        version: '1.5.10',
-        build: 94,
+        version: '1.5.11',
+        build: 96,
         download_url: 'https://github.com/4over7/SpeakOut/releases/latest',
         release_notes: '',
     });
