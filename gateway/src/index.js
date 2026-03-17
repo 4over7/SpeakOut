6bffda35301a4b8d1cd2e269cb3872d555789e71
LeonXu <desempty@gmail.com>
2026-03-17 21:08:24 +0800

    chore: build +97 (DMG)

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -23,7 +23,7 @@ app.options('*', (c) => c.body(null, 204));
 app.get('/version', (c) => {
     return c.json({
         version: '1.5.11',
-        build: 96,
+        build: 97,
         download_url: 'https://github.com/4over7/SpeakOut/releases/latest',
         release_notes: '',
     });
